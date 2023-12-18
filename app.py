dfb3a46d0ed31a440a508275446853958ad41f22
gdanylov <gdanylov@outlook.com>
2023-12-18 18:13:50 -0500

    render template for user input and styling

--- app.py
+++ app.py
@@ -11,4 +11,7 @@ def main():
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    return render_template('user_input.html', input_text=input_text)
+
+    
+    # return "You entered: " + input_text
